7c500039a0972d57da2277e1b937743e7815b35e
travis5 <Travis.in.indiana@gmail.com>
2015-08-07 20:52:37 -0400

    added am/pm clock

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,11 +1,19 @@
 function startTime() {
-    var today=new Date();
-    var h=today.getHours();
-    var m=today.getMinutes();
-    m=checkTime(m);
-    document.getElementById('clock').innerHTML=h+":"+m;
+  var todate = new Date();
+  var hh = todate.getHours();
+  var mm = todate.getMinutes();
+  var ss = todate.getSeconds();
+  var suffix= "AM";
+  if (hh > 12) {
+    hh = hh - 12;
+    suffix = "PM";
+  }
+  if (hh < 10) { hh = "0"+hh; }
+  if (mm < 10) { mm = "0"+mm; }
+  if (ss < 10) { ss = "0"+ss; }
+  document.getElementById('clock').innerHTML = hh+":"+mm + " " + suffix;
 
-    t=setTimeout('startTime()', 500)
+  t=setTimeout('startTime()', 500)
 }
 function checkTime(i) {
     if (i<10) {
